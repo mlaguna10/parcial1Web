2761d01812aa7fa4cc7fc2f73a2e862fe1185a78
Mateo Laguna <m.laguna10@uniandes.edu.co>
2022-10-11 10:58:41 -0500

    pruebas

--- src/cafe/cafe.service.spec.ts
+++ src/cafe/cafe.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CafeEntity } from './cafe.entity';
 import { CafeService } from './cafe.service';
+import { faker } from '@faker-js/faker';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('CafeService', () => {
   let service: CafeService;
@@ -12,7 +16,29 @@ describe('CafeService', () => {
     service = module.get<CafeService>(CafeService);
   });
 
+  service = module.get<CafeService>(CafeService);
+  repository = module.get<Repository<CafeEntity>>(getRepositoryToken(CafeEntity));
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create should return a new cafe', async () => {
+    const cafe: CafeEntity = {
+      id: 1,
+      nombre: faker.name.fullName(),
+      descripcion: faker.lorem.sentence(),
+      precio: 12,
+      tiendas: []
+    }
+  
+    const newCafe: CafeEntity = await service.createCafe(cafe);
+    expect(newCafe).not.toBeNull();
+  
+    const storedCafe: CafeEntity = await repository.findOne({where: {id: newCafe.id}})
+    expect(storedCafe).not.toBeNull();
+    expect(storedCafe.nombre).toEqual(newCafe.nombre)
+    expect(storedCafe.descripcion).toEqual(newCafe.descripcion)
+    expect(storedCafe.precio).toEqual(newCafe.precio)
   });
 });
--- src/tienda/tienda.service.spec.ts
+++ src/tienda/tienda.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TiendaEntity } from './tienda.entity';
 import { TiendaService } from './tienda.service';
 
 describe('TiendaService', () => {
@@ -12,7 +13,24 @@ describe('TiendaService', () => {
     service = module.get<TiendaService>(TiendaService);
   });
 
+  service = module.get<TiendaService>(TiendaService);
+  repository = module.get<Repository<TiendaEntity>>(getRepositoryToken(TiendaEntity));
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create should return a new tienda', async () => {
+    const tienda: TiendaEntity = {
+      id: 1,
+      nombre: faker.name.fullName(),
+      direccion: faker.lorem.sentence(),
+      telefono: 12,
+      cafes: []
+    }
+  
+    const newTienda: TiendaEntity = await service.createTienda(tienda);
+    expect(newTienda).not.toBeNull();
+
   });
 });
